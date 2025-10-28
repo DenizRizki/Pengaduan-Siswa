d44c0cc1ddf870b73794e00f1a1a64086d4797b8
mozarist <azzammozarist.xpro@gmail.com>
2025-10-28 14:54:15 +0700

    ui update (section and fixes)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,11 +19,11 @@
 </head>
 
 <body
-    class="bg-fixed bg-gradient-to-tr from-sky-100 via-white to-orange-50 text-blue-800 font-[Poppins] w-full min-h-screen flex flex-col items-center gap-5">
+    class="bg-fixed bg-gradient-to-tr from-sky-100 via-white to-sky-50 text-blue-800 font-[Poppins] w-full min-h-screen flex flex-col items-center gap-5">
     <div class="bg-white w-full max-w-7xl flex flex-col gap-10 min-h-screen border-x border-zinc-300">
         {{-- hero --}}
         <div
-            class="w-full min-h-[70vh] px-5 py-5 bg-gradient-to-tl from-blue-100 via-zinc-50 to-100 flex flex-col gap-5 justify-between border border-zinc-300 rounded-b-3xl">
+            class="w-full min-h-[70vh] px-5 py-5 bg-gradient-to-tl from-blue-100 via-zinc-50 to-sky-50 flex flex-col gap-5 justify-between border border-zinc-300 rounded-b-[34px]">
             <h1
                 class="bg-gradient-to-tr from-blue-900 via-blue-800 to-blue-600 bg-clip-text text-transparent max-w-4xl text-6xl md:text-7xl xl:text-8xl font-semibold">
                 Pengaduan Siswa SMK Informatika Pesat
@@ -34,7 +34,7 @@
                 </p>
 
                 <div class="flex gap-2 items-center">
-                    <a href="#"
+                    <a href="#form"
                         class="bg-transparent px-8 py-2 text-blue-800 border border-blue-800 rounded-full transition">
                         Adukan masalahmu!
                     </a>
@@ -47,16 +47,68 @@
         </div>
 
         {{-- main content --}}
-        <main class="space-y-10 px-5 py-5">
+        <main class="space-y-14 px-10 py-5">
 
-            <section class="flex flex-col gap-5 items-center">
+            <section class="flex flex-col gap-2 items-center">
                 <div class="space-y-2 text-center">
-                    <h2 class="text-4xl lg:text-6xl 2xl:text-6xl font-semibold uppercase">Ada masalah di sekolah?</h2>
+                    <h2 class="text-4xl lg:text-5xl 2xl:text-6xl font-semibold uppercase">Ada masalah di sekolah?</h2>
                     <p class="font-base leading-none">Keluhkan atau adukan masalahmu dengan mengisi form dibawah ini!
                     </p>
                 </div>
 
-                <x-form />
+                <div class="flex flex-col-reverse md:flex-row gap-5 items-center">
+                    <div class="space-y-14 w-full md:w-1/3">
+                        <div class="space-y-5">
+                            <h2 class="max-w-xl text-6xl font-semibold uppercase">Suaramu Penting!</h2>
+                            <p class="text-sm text-blue-900 leading-relaxed">
+                                Kami percaya setiap siswa berhak merasa aman dan nyaman di lingkungan sekolah.
+                                Melalui formulir ini, kamu bisa melaporkan kejadian yang tidak menyenangkan, memberi
+                                masukan, atau menyampaikan saran untuk membuat sekolah kita jadi tempat yang lebih baik.
+
+                                Jangan takut untuk berbicara. Laporanmu akan kami jaga kerahasiaannya.
+                            </p>
+                        </div>
+
+                        <div class="flex">
+                            <img src="https://images.unsplash.com/photo-1664786496152-c09bb24791b5?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"
+                                alt="gambar"
+                                class="relative bg-zinc-100 w-1/3 aspect-square object-cover border-4 border-white rounded-xl rotate-3">
+
+                            <img src="https://plus.unsplash.com/premium_photo-1661380704283-38adf12c3f5e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170"
+                                alt="gambar"
+                                class="relative bg-zinc-100 w-1/3 aspect-square object-cover border-4 border-white rounded-xl -left-2 -rotate-1">
+
+                            <img src="https://images.unsplash.com/photo-1509062522246-3755977927d7?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1532"
+                                alt="gambar"
+                                class="relative bg-zinc-100 w-1/3 aspect-square object-cover border-4 border-white rounded-xl -left-4 rotate-3">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-2/3 scroll-mt-5" id="form">
+                        <x-form />
+                    </div>
+                </div>
+            </section>
+
+            {{-- tentang kami --}}
+            <section class="flex flex-col md:flex-row gap-5 items-center justify-between">
+                <div class="space-y-5 max-w-2xl">
+                    <h2 class="text-6xl font-semibold uppercase">Tentang layanan pengaduan
+                        siswa
+                    </h2>
+                    <p class="text-sm text-blue-900 leading-relaxed">
+                        Layanan ini disediakan oleh SMK Informatika Pesat IT XPRO sebagai wadah bagi siswa untuk
+                        menyampaikan laporan terkait kejadian, pelanggaran, maupun masalah yang terjadi di lingkungan
+                        sekolah.
+
+                        Setiap laporan akan diverifikasi dan ditindaklanjuti secara profesional oleh pihak sekolah. Kami
+                        menjamin kerahasiaan data pelapor sesuai dengan kebijakan sekolah.
+                    </p>
+                </div>
+                <img src="https://images.unsplash.com/photo-1570616969692-54d6ba3d0397?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1122"
+                    alt="gambar"
+                    class="bg-zinc-100 w-full md:w-1/3 aspect-square object-cover border border-zinc-300 rounded-xl md:rotate-3">
+
             </section>
 
 
@@ -64,8 +116,8 @@
 
         <!-- FAQ -->
         <section id="FAQ"
-            class="bg-gradient-to-tl from-blue-100 via-zinc-50 to-blue-100 p-5 py-14 flex gap-5 items-start border-t border-zinc-300">
-            <div class="w-1/3 pt-8">
+            class="bg-gradient-to-tl from-blue-100 via-zinc-50 to-blue-100 p-10 py-14 flex flex-col md:flex-row gap-5 items-start border-t border-zinc-300">
+            <div class="w-full md:w-1/3 pt-8">
                 <h2 class="text-5xl font-semibold uppercase">
                     Punya pertanyaan tentang layanan kami?
                 </h2>
@@ -73,7 +125,7 @@
 
             {{-- accordion FAQ --}}
             <div
-                class="w-2/3 h-fit bg-white px-5 flex flex-col divide-y divide-zinc-300 border border-zinc-300 rounded-md">
+                class="w-full md:w-2/3 h-fit bg-white px-5 flex flex-col divide-y divide-zinc-300 border border-zinc-300 rounded-md">
                 <x-accordion content="1" title="1. Apa itu layanan pengaduan siswa?">
                     Layanan pengaduan siswa adalah sarana bagi seluruh siswa untuk menyampaikan keluhan, masukan, atau
                     laporan terkait lingkungan sekolah, baik mengenai fasilitas, perilaku, maupun sistem belajar, agar
