c514df557028ccf82cf46c7a84352a1592722ee2
mozarist <azzammozarist.xpro@gmail.com>
2025-10-28 10:58:20 +0700

    adding faq accordion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 
 <head>
     <meta charset="utf-8">
@@ -22,14 +22,16 @@
 </head>
 
 <body class="bg-zinc-50 text-zinc-900 font-[Geist] w-full min-h-screen flex flex-col items-center gap-5">
-    <div class="bg-blue-500 w-full max-w-7xl flex flex-col gap-10 min-h-screen border-x border-zinc-300">
+    <div class="bg-white w-full max-w-7xl flex flex-col gap-10 min-h-screen border-x border-zinc-300">
 
         {{-- hero --}}
         <div
-            class="text-white w-full min-h-[60vh] px-5 py-5 bg-gradient-to-tr from-zinc-950 via-zinc-950 to-blue-950 flex flex-col gap-5 justify-between rounded-b-3xl">
-            <h1 class="max-w-xl text-6xl md:text-7xl xl:text-8xl font-semibold">Pengaduan Siswa XPRO</h1>
+            class="text-white w-full min-h-[70vh] px-5 py-5 bg-gradient-to-tl from-blue-950 via-zinc-950 to-blue-950 flex flex-col gap-5 justify-between rounded-b-3xl">
+            <h1 class="max-w-4xl text-6xl md:text-7xl xl:text-8xl font-semibold">Pengaduan Siswa SMK Informatika Pesat
+                IT XPRO</h1>
             <div class="w-full flex flex-col md:flex-row gap-5 justify-between md:items-end">
-                <p class="max-w-sm text-lg md:text-2xl font-light leading-tight">Tempat aman bagi siswa untuk menyampaikan keluhan dan masukan.
+                <p class="max-w-sm text-lg md:text-2xl font-light leading-tight">Tempat aman bagi siswa untuk
+                    menyampaikan keluhan dan masukan.
                 </p>
 
                 <div class="flex gap-2 items-center">
@@ -37,7 +39,7 @@
                         Adukan masalahmu!
                     </a>
 
-                    <a href="#" class="bg-white px-8 py-2 text-zinc-900 border border-zinc-300 rounded-full">
+                    <a href="#FAQ" class="bg-white px-8 py-2 text-zinc-900 border border-zinc-300 rounded-full">
                         FAQ
                     </a>
                 </div>
@@ -55,9 +57,56 @@
                 </div>
             </section>
 
+
         </main>
 
+        <!-- FAQ -->
+        <section class="bg-zinc-100 p-5 py-14 flex gap-5 items-start">
+            <div class="w-1/3">
+                <h2 class="text-5xl font-semibold uppercase">
+                    Punya pertanyaan tentang layanan kami?
+                </h2>
+            </div>
+
+            {{-- accordion FAQ --}}
+            <div class="w-2/3 h-fit bg-white px-5 flex flex-col border border-zinc-300 rounded-md">
+                <x-accordion content="1" title="1. Apa itu layanan pengaduan siswa?">
+                    Layanan pengaduan siswa adalah sarana bagi seluruh siswa untuk menyampaikan keluhan, masukan, atau
+                    laporan terkait lingkungan sekolah, baik mengenai fasilitas, perilaku, maupun sistem belajar, agar
+                    dapat ditindaklanjuti oleh pihak sekolah
+                </x-accordion>
+
+                <x-accordion content="2" title="2. Siapa saja yang boleh menggunakan layanan ini?">
+                    Semua siswa aktif SMK Informatika Pesat dapat menggunakan layanan pengaduan ini, tanpa terkecuali.
+                    Setiap laporan akan
+                    dijaga kerahasiaannya untuk melindungi identitas pelapor.
+                </x-accordion>
+
+                <x-accordion content="3" title="3. Apakah pengaduan saya akan ditanggapi oleh pihak sekolah?">
+                    Ya, setiap laporan yang masuk akan diverifikasi oleh tim kesiswaan atau guru BK, lalu diteruskan
+                    kepada pihak yang berwenang untuk ditindaklanjuti sesuai jenis laporannya.
+                </x-accordion>
+
+                <x-accordion content="4" title="4. Apa saja jenis pengaduan yang bisa dilaporkan?">
+                    Siswa dapat melaporkan berbagai hal seperti:
+
+                    <ul>
+                        <li>1. Tindakan bullying atau kekerasan di lingkungan sekolah</li>
+
+                        <li>2. Fasilitas sekolah yang rusak atau kurang layak</li>
+
+                        <li>3. Ketidakadilan dalam perlakuan akademik atau non-akademik</li>
+
+                        <li>4. Keluhan terkait kegiatan sekolah lainnya</li>
+                    </ul>
+                </x-accordion>
+
+            </div>
+        </section>
+
     </div>
+
+
 </body>
 
 </html>
